3a8e167fde885e18b17a094633b26307931e9d65
RavirajPawar <pawarraviraj9160@gmail.com>
2025-10-02 11:11:16 +0530

    frequent commands

--- frequent-cmds.sh
+++ frequent-cmds.sh
@@ -1,2 +1,11 @@
 # convert line endings from windows to unix /r/n to /n
 dos2unix pull-models.sh
+
+# rebuild and restart docker containers
+docker compose down && docker compose up --build
+
+# fix ModuleNotFoundError: No module named 'some_module' by adding current directory to PYTHONPATH
+export PYTHONPTH=$PWD
+
+# activate virtual environment in windows
+source venv/Scripts/activate
